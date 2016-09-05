86ec13eac5326278a2ca433c2242588bf820119a
SG <sg@se7ensky.com>
2016-09-05 11:18:46 +0300

    npm dev script added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-console.log("Hello template")
+console.log("Hello template");
+document.write("fix");
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 		"./src"
 	],
 	output: {
-		path: './build',
+		path: path.resolve(__dirname, 'build'),
 		filename: 'bundle.js'
 	},
 	modules:{
